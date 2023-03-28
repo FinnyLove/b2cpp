a18653fc32972a03df60c15305cb8dedebc237bd
FinnyLove <125204541+FinnyLove@users.noreply.github.com>
2023-03-28 13:40:19 +0700

    Add files via upload

---
+++ b2cpp.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+struct HonSo
+{
+    int TuSo;
+    int MauSo;
+    int SoNguyen;
+};
+typedef struct HonSo HONSO;
+void Nhaphonso(HONSO &);
+void XuatHonSo(HONSO);
+void NhapHonSo(HONSO &h)
+{
+    printf("Nhap tu so: ");
+	scanf("%d", &h.Tu);
+    
+    printf("Nhap mau so: ");
+	scanf("%d", &h.Mau);
+
+	printf("Nhap so nguyen: ");
+	scanf("%d", &h.Nguyen);
+}
+void XuatHonSo(HONSO hs)
+{
+	printf("%d(%d/%d)", h.Nguyen, h.Tu, h.Mau);
+}
+int main()
+{
+	HONSO hs;
+	NhapHonSo(hs);
+	XuatHonSo(hs);
+	return 0;
+}
